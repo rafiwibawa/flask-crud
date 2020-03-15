d4ff9b7755ec68d1c83bb0135aa8c0ed505357b5
rafiwibawa <rafinew1997@gmail.com>
2020-03-15 19:37:59 +0700

    Membuat koneksi dan menambahkan folder static untuk menghubungkan ke boostrap

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, url_for, request, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
+import mysql.connector
 app = Flask(__name__)
 
+def opendb():
+    global mydb, mycursor
+    mydb = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='',
+        database='flaskdb'
+    )
+    mycursor = mydb.close()
+
+def closedb():
+    global mydb, mycursor
+    mycursor.close()
+    mydb.close()
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def view():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
